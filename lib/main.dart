d42161c34cb7c603f2911de65131b99194b035df
osama ahmed <osamagamil2070@gmail.com>
2024-07-21 02:27:25 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         primaryColor: Colors.blue,
         scaffoldBackgroundColor: Color(0xFFDFECDB),
@@ -30,6 +29,7 @@ class MyApp extends StatelessWidget {
           ),
           useMaterial3: false,
         ),
+<<<<<<< HEAD
         routes: {
           HomeLayout.routeName: (context) => HomeLayout(),
           LoginScreen.routeName: (context) => LoginScreen(),
@@ -42,6 +42,15 @@ class MyApp extends StatelessWidget {
         //HomeLayout.routeName,
         );
 
+=======
+        useMaterial3: false,
+      ),
+      routes: {
+        HomeLayout.routeName: (context) => HomeLayout(),
+        LoginScreen.routeName: (context) => LoginScreen()
+      },
+      initialRoute: HomeLayout.routeName,
+    );
+>>>>>>> 9b94453 (lec set)
   }
 }
-
